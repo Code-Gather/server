2125cd2a82e2b0db678f79b05dc1b03aebde35b8
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-04 19:21:11 +0900

    feat : add oauth token, oauth user info (github)

---
+++ src/main/java/com/codegather/authserver/oauth/DtoOfGitHubUserInfo.java
@@ -0,0 +1,16 @@
+package com.codegather.authserver.oauth;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DtoOfGitHubUserInfo {
+
+    private long id;
+
+    private String name;
+}
---
+++ src/main/java/com/codegather/authserver/oauth/DtoOfOAuthToken.java
@@ -0,0 +1,17 @@
+package com.codegather.authserver.oauth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DtoOfOAuthToken {
+
+    private String accessToken;
+
+    private String refreshToken;
+}
